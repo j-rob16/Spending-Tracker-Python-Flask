5978610dedbee0b5bc2ffddb374b63744e8cddf9
James R <jimmer1609@gmail.com>
2022-06-13 20:08:15 +0900

    merchant controller and form added

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
+from controllers.merchants_controller import merchants_blueprint
 from controllers.transaction_controller import transactions_blueprint
 from controllers.user_controller import users_blueprint
 
+
 app = Flask(__name__)
 
+app.register_blueprint(merchants_blueprint)
 app.register_blueprint(users_blueprint)
 app.register_blueprint(transactions_blueprint)
 
---
+++ controllers/merchants_controller.py
@@ -0,0 +1,29 @@
+from crypt import methods
+from flask import Flask, request, render_template, redirect
+from flask import Blueprint
+
+from models.merchant import Merchant
+import repositories.merchant_repository as merchant_repo
+
+merchants_blueprint = Blueprint('merchants', __name__)
+
+@merchants_blueprint.route('/merchants')
+def merchants():
+    all_merchants = merchant_repo.select_all()
+    return render_template('merchants/index.html', merchants=all_merchants)
+
+@merchants_blueprint.route('/merchants/<id>')
+def show_merchant(id):
+    merchant = merchant_repo.select(id)
+    return render_template('/merchants/shop.html', merchant=merchant)
+
+@merchants_blueprint.route('/merchants/new')
+def new_merchant():
+    return render_template('/merchants/new.html')
+
+@merchants_blueprint.route('/merchants', methods=['POST'])
+def create_merchant():
+    name = request.form['name']
+    merchant = Merchant(name)
+    merchant_repo.save(merchant)
+    return redirect('/merchants')
